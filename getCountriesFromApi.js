d0ee797b69f001f176c1137ae150d60196765533
aleksanderbr <alexanderbraun@freenet.de>
2020-06-24 18:02:37 +0200

    formatted

--- getCountriesFromApi.js
+++ getCountriesFromApi.js
@@ -1,37 +1,38 @@
-const https = require('https');
+const https = require("https");
 const url = "https://jsonmock.hackerrank.com/api/countries?page=";
 
-let callBack = function(data){
+let callBack = function (data) {
   console.log(data);
 };
-const getData = function (pageNo=1, someCallBack, countryCode) {
+const getData = function (pageNo = 1, someCallBack, countryCode) {
   let actualUrl = url + pageNo;
-  let mydata = []
-  https.get(actualUrl, (resp) => {
-    let data = '';
-    resp.on('data', (chunk) => {
+  let mydata = [];
+  https
+    .get(actualUrl, (resp) => {
+      let data = "";
+      resp.on("data", (chunk) => {
         data += chunk;
-    });
-    resp.on('end', async () => {
-        if (JSON.parse(data).total_pages >= pageNo){
-          pageNo+=1;
+      });
+      resp.on("end", async () => {
+        if (JSON.parse(data).total_pages >= pageNo) {
+          pageNo += 1;
           let countries = JSON.parse(data).data;
           let found = false;
-          for(let country of countries) {
-            if(country.alpha2Code === countryCode) {
+          for (let country of countries) {
+            if (country.alpha2Code === countryCode) {
               found = true;
-              return callBack(country.name)
+              return callBack(country.name);
             }
           }
-          if(!found) {
-            await getData(pageNo, callBack, countryCode); 
+          if (!found) {
+            await getData(pageNo, callBack, countryCode);
           }
         }
-    });
-  }).on("error", (err) => {
+      });
+    })
+    .on("error", (err) => {
       console.log("Error: " + err.message);
-  });
-}
+    });
+};
 
-getData(pageNo=1, callBack, 'DE');
-
+getData((pageNo = 1), callBack, "DE");
